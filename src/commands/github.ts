b3d1a6f2cc443cd54799f1ecc71facb086dbcd6f
AlexMan123456 <alex.man03888@gmail.com>
2025-07-26 19:37:40 +0100

    Add a link to this Git repository on GitHub

---
+++ src/commands/github.ts
@@ -0,0 +1,34 @@
+import {
+  ApplicationCommandRegistry,
+  Awaitable,
+  Command,
+} from "@sapphire/framework";
+import { ChatInputCommandInteraction } from "discord.js";
+
+class GitHubLinkCommand extends Command {
+  public constructor(context: Command.LoaderContext, options: Command.Options) {
+    super(context, { ...options });
+  }
+
+  public override registerApplicationCommands(
+    registry: ApplicationCommandRegistry,
+  ): Awaitable<void> {
+    registry.registerChatInputCommand((builder) => {
+      return builder
+        .setName("github")
+        .setDescription(
+          "Respond with GitHub repository link to bot source code",
+        );
+    });
+  }
+
+  public override async chatInputRun(
+    interaction: ChatInputCommandInteraction,
+  ): Promise<void> {
+    await interaction.reply(
+      "Here's the GitHub repository link to my source code: https://github.com/AlexMan123456/alex-g-bot-2",
+    );
+  }
+}
+
+export default GitHubLinkCommand;
