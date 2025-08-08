a33ef7ccae026ba7583f949ae55e6fcb6c77054f
Badstagram <hello@badstagram.gay>
2025-08-08 21:52:08 +0100

    Add update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+version=$(jq -r '.version' < package.json)
+
+git pull
+docker build -t alex-g-bot-2:$version 
+
+docker stop alex-g-bot-2
+docker rm alex-g-bot-2
+
+docker run -d --name alex-g-bot-2 alex-g-bot:$version
