dc0438d733fa017f76d126036f30c52abe342038
AlexMan123456 <alex.man03888@gmail.com>
2025-08-08 22:03:19 +0100

    Add missing 2 because Badstagram can't stop making dumb typos

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ docker build -t alex-g-bot-2:$version .
 docker stop alex-g-bot-2
 docker rm alex-g-bot-2
 
-docker run -d --name alex-g-bot-2 alex-g-bot:$version
+docker run -d --name alex-g-bot-2 alex-g-bot-2:$version
